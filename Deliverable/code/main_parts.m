d7b40f7c2e2073443df945bb95e6af6edc5a9a7a
Nick-Ol <clement.nicolle@student.ecp.fr>
2015-01-08 16:07:28 +0100

    Logistic regression

--- Deliverable/code/main_parts.m
+++ Deliverable/code/main_parts.m
@@ -87,6 +87,21 @@ switch lower(classifier_name)
     case 'linear'    %% I can do this 
         w_linear = (labels*features')/(features*features');
     case 'logistic'  %% you do the rest 
+        w_logistic = zeros(size(features,2),1);
+        while 1 %% continue until convergence criterion is met 
+            w_prev = w;
+
+            % update w (Newton-Raphson)
+            J = gradient(w_prev, Y, X, 0);
+            H = hessian(w_prev, X, 0);
+            w = w_prev - H\J';
+  
+            % convergence criterion
+            if sqrt(sum((w-w_prev).^2)/ sqrt(sum(w.^2)))<.001
+                break
+            end
+        end
+        
     case 'adaboost'
     case 'svm'
     case 'svm-rbf'
