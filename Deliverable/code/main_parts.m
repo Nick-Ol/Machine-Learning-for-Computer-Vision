b4fee485dc0e880386b6abe6751160479277863f
Nick-Ol <clement.nicolle@student.ecp.fr>
2015-01-20 16:05:33 +0100

    Loops for a given classifier

--- Deliverable/code/main_parts.m
+++ Deliverable/code/main_parts.m
@@ -1,6 +1,6 @@
 %% addpaths
 this_dir = fileparts(mfilename('fullpath')); addpath(this_dir); addpath(fullfile(this_dir,'steerable'));
-run('/Users/Mathurin/Documents/MATLAB/VLFEATROOT/toolbox/vl_setup')
+%run('/Users/Mathurin/Documents/MATLAB/VLFEATROOT/toolbox/vl_setup')
 addpath('util/');
 
 %% seeding
@@ -23,12 +23,12 @@ hold on,scatter(pts(1,:),pts(2,:),'r','filled');
 %--------------------------------
 
 %% images with faces
-total_positives = 200;      %% set to 1500 by the end
+total_positives = 150;      %% set to 1500 by the end
 train_positives = [1:2:total_positives];
 test_positives  = [2:2:total_positives];
 
 %% background images (seem few, but we draw multiple negatives samples per image)
-total_negatives = 10;       %% set to 200 by the end
+total_negatives = 20;       %% set to 200 by the end
 train_negatives = [1:2:total_negatives];
 test_negatives  = [2:2:total_negatives];
 
@@ -44,172 +44,171 @@ test_lbl  = [ones(size(test_positives)), zeros(1,length(test_negatives))];
 %--------------------------------
 
 feature_names       = {'SIFT','STEER'};
-feature_ind         = 1; %% change to use steerable filter responses
-feat                = feature_names{feature_ind};
-
 part_names          = {'Left eye','Right eye','Left mouth','Right mouth','Nose'};
-
-part                = 1; %% change to train classifiers for other parts 
-part_name           = part_names{part};
-
 classifier_names    = {'Linear','Logistic','SVM','SVM-RBF','Adaboost'};
-classifier          = 1;
+classifier          = 1; %% change the classifier here
 classifier_name     = classifier_names{classifier};
+for feature_ind = 1:2
+    feat                = feature_names{feature_ind};
+    %part                = 1; %% change to train classifiers for other parts 
+    for part =1:5
+        part_name           = part_names{part};
 
-%% use this string to save your classifiers 
-%experiment_string = sprintf('Features_%s_Part_%s_Classifier_%s',feat,part_name,classifier_name);
+        %% use this string to save your classifiers 
+        %experiment_string = sprintf('Features_%s_Part_%s_Classifier_%s',feat,part_name,classifier_name);
 
-%-------------------------------------------------------------------
-%% training
-%-------------------------------------------------------------------
+        %-------------------------------------------------------------------
+        %% training
+        %-------------------------------------------------------------------
 
-%% Step 1: gather dataset 
-normalize = 1;  % makes sure faces come at a fixed scale
-features  = [];
-labels    = [];
-fprintf('Gathering training set: \n0+ ');
+        %% Step 1: gather dataset 
+        normalize = 1;  % makes sure faces come at a fixed scale
+        features  = [];
+        labels    = [];
+        fprintf('Gathering training set: \n0+ ');
 
-for im_idx = [1:length(train_set)]
-    image_id  = train_set(im_idx);
-    image_lb  = train_lbl(im_idx);
-    
-    [input_image,points]    = load_im(image_id,image_lb,normalize,part);
-    features_im             = get_features(input_image,feat,points);
-    reject = any((isnan(features_im)|isinf(features_im)),1); 
-    features_im(:,reject) = [];
+        for im_idx = [1:length(train_set)]
+            image_id  = train_set(im_idx);
+            image_lb  = train_lbl(im_idx);
 
-    features                = [features,features_im];
-    labels                  = [labels,  image_lb*ones(1,size(features_im,2))];
-    
-    fprintf(2,' %i',mod(im_idx-1,10));
-    if mod(im_idx,10)==0, fprintf(2,'\n%i+ ',im_idx); end
-end
+            [input_image,points]    = load_im(image_id,image_lb,normalize,part);
+            features_im             = get_features(input_image,feat,points);
+            reject = any((isnan(features_im)|isinf(features_im)),1); 
+            features_im(:,reject) = [];
+
+            features                = [features,features_im];
+            labels                  = [labels,  image_lb*ones(1,size(features_im,2))];
+
+            fprintf(2,' %i',mod(im_idx-1,10));
+            if mod(im_idx,10)==0, fprintf(2,'\n%i+ ',im_idx); end
+        end
 
 
-%% Step 2: train classifier 
+        %% Step 2: train classifier 
 
-switch lower(classifier_name)
-    case 'linear'    %% I can do this 
-        w_linear = (labels*features')/(features*features');
-    case 'logistic'  %% you do the rest 
-        w_logistic = log_reg(features', labels')';
-        
-    case 'adaboost'
-        [alpha,coord,polarity,theta] = adaboost(200, features, labels);
+        switch lower(classifier_name)
+            case 'linear'    %% I can do this 
+                w_linear = (labels*features')/(features*features');
+            case 'logistic'  %% you do the rest 
+                w_logistic = log_reg(features', labels')';
 
-	case 'svm'
-        perm = randperm(size(features,2));
-        features_perm = features(:, perm);
-        labels_perm = labels(:, perm); % dispatch the labels
-        Ncosts  = 20;
-        cost_range = logsample(.1,1000,Ncosts);
-        best_cost_lin = cross_val_linear_svm(10, cost_range, features_perm', labels_perm');
-    	w_lin_svm = linear_svm(features', labels', best_cost_lin);
-	
-    case 'svm-rbf'
-        perm = randperm(size(features,2));
-        features_perm = features(:, perm);
-        labels_perm = labels(:, perm); % dispatch the labels
-        Ngammas = 20;
-        Ncosts  = 20;
-        gamma_range = logsample(.1,1000,Ngammas);
-        cost_range  = logsample(.1,1000,Ncosts);
-        [best_cost_rbf, best_gamma] = cross_val_rbf_svm(10, cost_range, gamma_range, features_perm', labels_perm');
-        w_rbf_svm = rbf_svm(features', labels', best_gamma, best_cost_rbf);
-end
+            case 'adaboost'
+                [alpha,coord,polarity,theta] = adaboost(200, features, labels);
 
-%% fun code: see what the classifier wants to see 
-if 0
-figure,
-vl_plotsiftdescriptor(max(w_linear(1:end-1)',0)); 
-title('positive components of weight vector');
+            case 'svm'
+                perm = randperm(size(features,2));
+                features_perm = features(:, perm);
+                labels_perm = labels(:, perm); % dispatch the labels
+                Ncosts  = 20;
+                cost_range = logsample(.1,1000,Ncosts);
+                best_cost_lin = cross_val_linear_svm(10, cost_range, features_perm', labels_perm');
+                w_lin_svm = linear_svm(features', labels', best_cost_lin);
 
-figure,
-vl_plotsiftdescriptor(max(-w_linear(1:end-1)',0));
-title('negative  components of weight vector');
-end
+            case 'svm-rbf'
+                perm = randperm(size(features,2));
+                features_perm = features(:, perm);
+                labels_perm = labels(:, perm); % dispatch the labels
+                Ngammas = 20;
+                Ncosts  = 20;
+                gamma_range = logsample(.1,1000,Ngammas);
+                cost_range  = logsample(.1,1000,Ncosts);
+                [best_cost_rbf, best_gamma] = cross_val_rbf_svm(10, cost_range, gamma_range, features_perm', labels_perm');
+                w_rbf_svm = rbf_svm(features', labels', best_gamma, best_cost_rbf);
+        end
 
-%-------------------------------------------------------------------
-%% testing
-%-------------------------------------------------------------------
-%% Step 1: gather dataset 
-normalize = 1;  % makes sure faces come at a fixed scale
-features  = [];
-labels    = [];
-fprintf('Gathering test set: \n0 +');
+        %% fun code: see what the classifier wants to see 
+        if 0
+        figure,
+        vl_plotsiftdescriptor(max(w_linear(1:end-1)',0)); 
+        title('positive components of weight vector');
 
-for im_idx = [1:length(test_set)]
-    image_id  = test_set(im_idx);
-    image_lb  = test_lbl(im_idx);
-    
-    [input_image,points]    = load_im(image_id,image_lb,normalize,part);
-    features_im             = get_features(input_image,feat,points);
-    
-    reject = any((isnan(features_im)|isinf(features_im)),1); features_im(:,reject) = [];   
-    features                = [features,features_im];
-    
-    labels                  = [labels,  image_lb*ones(1,size(points,2))];
-    
-    fprintf(2,'.%i',mod(im_idx-1,10));
-    if mod(im_idx,10)==0, fprintf(2,'\n%i+ ',floor(im_idx/10)); end
-end
+        figure,
+        vl_plotsiftdescriptor(max(-w_linear(1:end-1)',0));
+        title('negative  components of weight vector');
+        end
 
-%% Step 2: Precision recall curve for classifier (your code here);
-if 0
-thresholds = [-2:.01:2];
-switch lower(classifier_name)
-        case 'linear'
-            [precision, recall] = precision_recall_w(w_linear*features, thresholds, labels');
+        %-------------------------------------------------------------------
+        %% testing
+        %-------------------------------------------------------------------
+        %% Step 1: gather dataset 
+        normalize = 1;  % makes sure faces come at a fixed scale
+        features  = [];
+        labels    = [];
+        fprintf('Gathering test set: \n0 +');
 
-         case 'logistic'
-         	[precision, recall] = precision_recall_w(w_logistic*features, thresholds, labels');
+        for im_idx = [1:length(test_set)]
+            image_id  = test_set(im_idx);
+            image_lb  = test_lbl(im_idx);
 
-         case 'svm'
-         	[precision, recall] = precision_recall_w(w_lin_svm*features, thresholds, labels');
+            [input_image,points]    = load_im(image_id,image_lb,normalize,part);
+            features_im             = get_features(input_image,feat,points);
 
-         case 'svm-rbf'
-         	[precision, recall] = precision_recall_w(w_rbf_svm*features, thresholds, labels');
-           
-         case 'adaboost'
-            scores = adaboost_scores(polarity, features, coord, theta, alpha);
-            [precision, recall] = precision_recall_w(scores, thresholds, labels');
-        
+            reject = any((isnan(features_im)|isinf(features_im)),1); features_im(:,reject) = [];   
+            features                = [features,features_im];
 
-end
+            labels                  = [labels,  image_lb*ones(1,size(points,2))];
 
-title_string    = sprintf('%s precision-recall for part: %s',classifier_name,part_name);
-figure;
-plot(precision,recall); axis([0,1,0,1]);
-title(title_string); xlabel('Precision'); ylabel('Recall');
-end
-%% Step 3: Dense evaluation of classifier
-    
-for image_lb = [0,1] 
-    %% try both a positive and a negative image
-    [input_image,points]       = load_im(image_id,image_lb,normalize,part);
-    
-    %% important: make sure you do NOT give the third, 'points', argument
-    [dense_features,crds,idxs] = get_features(input_image,feat);
-    
-    %% dense_features will be Nfeats X Npoints. For linear classifiers:
-    % (adapt accordingly for Adaboost)
-    
-    switch lower(classifier_name)
-        case 'linear',
-            score_classifier = w_linear*dense_features;
-        case 'logistic'
-            score_classifier = w_logistic*dense_features;
-        case 'svm'
-            score_classifier = w_lin_svm*dense_features;
-        case 'svm-rbf'
-            score_classifier = w_rbf_svm*dense_features;
-        case 'adaboost'
+            fprintf(2,'.%i',mod(im_idx-1,10));
+            if mod(im_idx,10)==0, fprintf(2,'\n%i+ ',floor(im_idx/10)); end
+        end
+
+        %% Step 2: Precision recall curve for classifier (your code here);
+
+        thresholds = [-2:.01:2];
+        switch lower(classifier_name)
+                case 'linear'
+                    [precision, recall] = precision_recall_w(w_linear*features, thresholds, labels');
+
+                 case 'logistic'
+                    [precision, recall] = precision_recall_w(w_logistic*features, thresholds, labels');
+
+                 case 'svm'
+                    [precision, recall] = precision_recall_w(w_lin_svm*features, thresholds, labels');
+
+                 case 'svm-rbf'
+                    [precision, recall] = precision_recall_w(w_rbf_svm*features, thresholds, labels');
+
+                 case 'adaboost'
+                    scores = adaboost_scores(polarity, features, coord, theta, alpha);
+                    [precision, recall] = precision_recall_w(scores, thresholds, labels');
+
+        end
+
+        title_string    = sprintf('%s precision-recall for part: %s',classifier_name,part_name);
+        figure;
+        plot(precision,recall); axis([0,1,0,1]);
+        title(title_string); xlabel('Precision'); ylabel('Recall');
+
+        %% Step 3: Dense evaluation of classifier
+
+        for image_lb = [0,1] 
+            %% try both a positive and a negative image
+            [input_image,points]       = load_im(image_id,image_lb,normalize,part);
+
+            %% important: make sure you do NOT give the third, 'points', argument
+            [dense_features,crds,idxs] = get_features(input_image,feat);
+
+            %% dense_features will be Nfeats X Npoints. For linear classifiers:
+            % (adapt accordingly for Adaboost)
+
+            switch lower(classifier_name)
+                case 'linear',
+                    score_classifier = w_linear*dense_features;
+                case 'logistic'
+                    score_classifier = w_logistic*dense_features;
+                case 'svm'
+                    score_classifier = w_lin_svm*dense_features;
+                case 'svm-rbf'
+                    score_classifier = w_rbf_svm*dense_features;
+                case 'adaboost'
+            end
+            [sv,sh]     = size(input_image);
+            score       = -inf*ones(sv,sh);
+            score(idxs) = score_classifier;
+
+            title_string    = sprintf('%s score for part: %s',classifier_name,part_name);
+            figure,imagesc(score,[min(score_classifier),max(score_classifier)]); title(title_string);
+            figure,imshow(input_image);
+        end
     end
-    [sv,sh]     = size(input_image);
-    score       = -inf*ones(sv,sh);
-    score(idxs) = score_classifier;
-    
-    title_string    = sprintf('%s score for part: %s',classifier_name,part_name);
-    figure,imagesc(score,[min(score_classifier),max(score_classifier)]); title(title_string);
-    figure,imshow(input_image);
 end
